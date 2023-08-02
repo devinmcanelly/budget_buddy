249ee4e0a633b82f58a27fa5ca7fd55096ebf4f4
Devin McAnelly <devinmcanelly@outlook.com>
2023-08-02 04:00:41 -0500

    saving before sqlachemy.

--- app/db.py
+++ app/db.py
@@ -1,14 +1,16 @@
 import sqlite3
 import transactions
+
+
 def initialize():
     con = sqlite3.connect("budget.db")
     cur = con.cursor()
-    
+
     # Create table if not exists
-    
+
     cur.execute('''
         CREATE TABLE IF NOT EXISTS accounts(
-            account_id INTEGER PRIMARY KEY,
+            account_id INTEGER NOT NULL PRIMARY KEY,
             balance REAL,
             account_type TEXT,
             account_name TEXT
@@ -17,7 +19,7 @@ def initialize():
                 ''')
     cur.execute("""
         CREATE TABLE IF NOT EXISTS transactions(
-            transaction_id INTEGER PRIMARY KEY,
+            transaction_id INTEGER NOT NULL PRIMARY KEY,
             amount REAL,
             account_id INTEGER,
             date TEXT,
@@ -30,19 +32,21 @@ def initialize():
     con.commit()
     con.close()
 
+
 def record(self):
     con = sqlite3.connect("budget.db")
     cur = con.cursor()
     query = """
                 INSERT INTO transactions (amount, account_id, date, memo, category, payee)
                 VALUES (?, ?, ?, ?, ?, ?)
-    """ 
+    """
     cur.execute(query, (self.amount, self.account, self.date, self.memo, self.category, self.payee))
     con.commit()
     con.close()
-    
 
-def create_account(self):
-    con = sqlite3.connect("budget.db")
-    cur = con.cursor()
-    query
+# def create_account(self):
+#     con = sqlite3.connect("budget.db")
+#     cur = con.cursor()
+#     query = """
+#         INSERT INTO account
+#     """
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import usr_input as ui
 import db
 
-
 db.initialize()
-
+# account creation 
+# app starts up and checks for records, if none start account initialization. O
 # ui.menu()
 
 ui.get_new()
